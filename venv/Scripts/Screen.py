f98781f2cd2d6c172879925c9775bf131bb9e440
ChaniCohen7 <83139269+ChaniCohen7@users.noreply.github.com>
2021-07-30 03:13:37 +0300

    p
    second

--- venv/Scripts/Screen.py
+++ venv/Scripts/Screen.py
@@ -1,9 +1,18 @@
-import turtle
+from tkinter import *
 
+def click():
+    val = convert_from["text"]
+    print(val)
+f = Frame()
+convert_from = Label(f, text="convert_from: ", anchor="w")
+convert_from.pack(fill="x")
+convert_from = Entry(f)
+convert_from.pack()
+convert_to = Label(f, text="convert_to: ", anchor="w")
+convert_to.pack(fill="x")
+convert_to = Entry(f)
+convert_to.pack()
+money = Label(f, text="money: ", anchor="w")
+money.pack(fill="x")
+money = Entry(f)
 
-wn=turtle.Screen()
-p=turtle.Turtle()
-
-turtle.mainloop()
-
-#tkinter
